fefb551ffdfde9a971c3a660152ef3bb4f1476ed
TahminaMammad <tahminamammad05@gmail.com>
2025-12-13 22:28:20 +0000

    Fix: Shorten module docstring to comply with PEP8 line length

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 """Defines a Rectangle class with area, perimeter, and string output."""
-
 class Rectangle:
     """Defines a rectangle with width and height properties."""
-
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-
     @property
     def width(self):
         return self.__width
-
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -19,11 +15,9 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-
     @property
     def height(self):
         return self.__height
-
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -31,15 +25,12 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
     def area(self):
         return self.__width * self.__height
-
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
-
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
