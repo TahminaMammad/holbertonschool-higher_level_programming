e401c172cf9bac9c4ca37393cd7f182487a38f2a
TahminaMammad <tahminamammad05@gmail.com>
2025-12-14 07:51:53 +0000

    Task4: Implement inherits_from function

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Check if an object inherits from a specified class."""
+
+
+def inherits_from(obj, a_class):
+    """Return True if obj is an instance of a subclass of a_class.
+
+    Otherwise return False.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
