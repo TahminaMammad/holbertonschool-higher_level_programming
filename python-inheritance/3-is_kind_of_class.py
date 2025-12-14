a8b66cbaedd66e1d4689a83624d5b2b8e80c5c17
TahminaMammad <tahminamammad05@gmail.com>
2025-12-14 07:50:01 +0000

    Task3: Fix PEP8 and implement is_kind_of_class

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Defines a function to check class inheritance."""
+"""Check if an object is an instance or a subclass of a class."""
 
 
 def is_kind_of_class(obj, a_class):
-    """Return True if obj is an instance or inherits from a_class, else False."""
+    """Return True if obj is an instance or subclass of a_class, else False."""
     return isinstance(obj, a_class)
