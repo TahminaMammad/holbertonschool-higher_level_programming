cedfc0b5d986654508ddef32fe2133797251bcdb
TahminaMammad <tahminamammad05@gmail.com>
2025-12-14 09:23:07 +0000

    Task10: Implement Student class with to_json filtering

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a Student class with JSON serialization and filtering."""
+
+
+class Student:
+    """Class that defines a student by first name, last name, and age."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return the dictionary representation of the Student instance.
+
+        If attrs is a list of strings, only attributes with names in this list
+        are included. Otherwise, all attributes are returned.
+        """
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
+        return self.__dict__
