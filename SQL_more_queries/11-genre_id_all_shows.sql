9dfaa6a45aadad1d741788dfd69734df7ea37f8c
TahminaMammad <tahminamammad05@gmail.com>
2025-12-21 12:10:49 +0000

    Add script to list all shows with genre IDs (NULL if none)

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,11 @@
+-- 11-genre_id_all_shows.sql
+-- Script that lists all shows contained in hbtn_0d_tvshows
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- If a show doesn’t have a genre, display NULL
+-- Only one SELECT statement
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
