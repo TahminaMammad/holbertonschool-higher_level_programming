11e0c89f9c4734084db53d903e5baf91699cf6b0
TahminaMammad <tahminamammad05@gmail.com>
2025-12-21 12:08:46 +0000

    Add script to list shows with their genre IDs

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,10 @@
+-- 10-genre_id_by_show.sql
+-- Script that lists all shows contained in hbtn_0d_tvshows
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- Only one SELECT statement
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows, tv_show_genres
+WHERE tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
