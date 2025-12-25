aca5f43db80b45591e3d4901e98194e5a726a8cb
TahminaMammad <tahminamammad05@gmail.com>
2025-12-25 07:09:04 -0800

    Refactor alphabet printing to use ord() and chr()

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i not in (101, 113):
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) not in ('q', 'e'):
         print("{}".format(chr(i)), end="")
